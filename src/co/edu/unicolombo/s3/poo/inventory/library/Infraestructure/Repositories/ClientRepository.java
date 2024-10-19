3c7c2a6c662456358e9155d9324cea7ae1d92796
joseamayamorelos <jose.amayamorelos@unicolombo.edu.co>
2024-10-19 15:05:51 -0500

    creation of repository client and add new methods to client

---
+++ src/co/edu/unicolombo/s3/poo/inventory/library/Infraestructure/Repositories/ClientRepository.java
@@ -0,0 +1,65 @@
+package co.edu.unicolombo.s3.poo.inventory.library.Infraestructure.Repositories;
+
+import co.edu.unicolombo.s3.poo.inventory.library.Domain.Models.Client;
+import co.edu.unicolombo.s3.poo.inventory.library.Infraestructure.Persistences.DB;
+import co.edu.unicolombo.s3.poo.inventory.library.Service.Interfaces.Repositories.IClientRepository;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author Jose
+ */
+public class ClientRepository implements IClientRepository {
+
+    private final DB db = DB.getInstance();
+
+    @Override
+    public void addClient(Client client) throws Exception {
+        if (client == null) {
+            throw new Exception("The client can not created");
+        }
+        db.getListClients().add(client);
+    }
+
+    @Override
+    public List<Client> getListClients() throws Exception {
+        if (db.getListClients() == null || db.getListClients().isEmpty()) {
+            throw new Exception();
+        }
+        return db.getListClients();
+    }
+
+    @Override
+    public Client getClientByName(String name) throws Exception {
+        if (name == null) {
+            throw new Exception("name is null");
+        }
+        Optional<Client> firstClient = db.getListClients()
+                .stream()
+                .filter(cl -> cl.getName().equalsIgnoreCase(name))
+                .findFirst();
+        if (!firstClient.isPresent()) {
+            throw new Exception("client not found");
+        }
+        return firstClient.get();
+    }
+
+    @Override
+    public void updateClient(Client newClient) throws Exception {
+        if (newClient == null) {
+            throw new Exception("the client is null or id invalid");
+        }
+        var indexClient = db.getIndexClientById(newClient.getID());
+        db.getListClients().set(indexClient, newClient);
+    }
+
+    @Override
+    public void deleteClient(Client client) {
+        if (client != null) {
+            var indexClient = db.getIndexClientById(client.getID());
+            db.getListClients().remove(indexClient);
+        }
+    }
+
+}
