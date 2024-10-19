13214dbb245d3c879b3f66c5206fb20a8652f1db
Jean-Pierre <jean.galvismiranda@unicolombo.edu.co>
2024-10-19 15:48:24 -0500

    add gets clients

---
+++ src/co/edu/unicolombo/s3/poo/inventory/library/Service/Controller/Queries/Client/GetAllClientsQueries.java
@@ -0,0 +1,27 @@
+
+package co.edu.unicolombo.s3.poo.inventory.library.Service.Controller.Queries.Client;
+
+import java.util.List;
+
+import co.edu.unicolombo.s3.poo.inventory.library.Domain.Models.Client;
+import co.edu.unicolombo.s3.poo.inventory.library.Infraestructure.Repositories.ClientRepository;
+import co.edu.unicolombo.s3.poo.inventory.library.Service.Interfaces.Queries.Client.IGetListClientQueries;
+
+/**
+ *
+ * @author Jean
+ */
+public class GetAllClientsQueries implements IGetListClientQueries {
+
+    private ClientRepository clientRepository;
+
+    public GetAllClientsQueries(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public List<Client> getListClients() throws Exception {
+        return clientRepository.getListClients();
+    }
+
+}
